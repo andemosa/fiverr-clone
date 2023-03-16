8c16c4e4052620fdf00bc2f889ea8b82e8cb7ec5
andemosa <osayerieanderson@yahoo.com>
2023-03-16 09:04:27 +0100

    feat: Client - Messages section

--- client/src/pages/Message.tsx
+++ client/src/pages/Message.tsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom"
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <section className="message">
+      <div className="message__container">
+        <span className="message__breadcrumbs">
+          <Link to="/messages">Messages</Link> &gt; John Doe &gt;
+        </span>
+        <div className="message__messages">
+            {Array.from({ length: 5 }).map((_, i) => (
+              <div className={i % 2 ? "owner item" : "item"} key={i}>
+                <img
+                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt=""
+                />
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam cupiditate, sunt voluptates tempora suscipit, neque molestiae excepturi sapiente fuga quidem modi quam accusamus consectetur quod facilis enim adipisci rerum nobis.</p>
+              </div>
+            ))}
+          </div>
+        <hr />
+        <form>
+          <textarea placeholder="write a message" />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- client/src/pages/Messages.tsx
+++ client/src/pages/Messages.tsx
@@ -1,8 +1,35 @@
+import { Link } from "react-router-dom";
 
 const Messages = () => {
   return (
-    <div>Messages</div>
-  )
-}
+    <section className="messages">
+      <div className="messages__container">
+        <div className="messages__title">
+          <h1>Messages</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          {Array.from({ length: 5 }).map((_, i) => (
+            <tr className="messages__active" key={i}>
+              <td>id</td>
+              <td>
+                <Link to={`/message/3`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci doloremque modi amet exercitationem explicabo, ipsa temporibus incidunt similique voluptate possimus tempora harum corporis nemo facilis aut. Nobis itaque temporibus deleniti?</Link>
+              </td>
+              <td>time</td>
+              <td>
+                <button>Mark as Read</button>
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </section>
+  );
+};
 
-export default Messages
+export default Messages;
