ce0504614f47e5cfeb776355af1919cb6f2aa403
andemosa <osayerieanderson@yahoo.com>
2023-03-17 08:30:37 +0100

    feat: Client - My gigs page

--- client/src/pages/MyGigs.tsx
+++ client/src/pages/MyGigs.tsx
@@ -1,8 +1,40 @@
+import { Link } from "react-router-dom";
 
 const MyGigs = () => {
   return (
-    <div>MyGigs</div>
-  )
-}
+    <section className="myGigs">
+      <div className="myGigs__container">
+        <div className="myGigs__title">
+          <h1>Gigs</h1>
+          <Link to="/add">
+            <button>Add New Gig</button>
+          </Link>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Sales</th>
+            <th>Action</th>
+          </tr>
+          {Array.from({ length: 5 }).map((_, i) => (
+            <tr key={i}>
+              <td>
+                <img className="myGigs__image" src={"/img/noavatar.webp"} alt="" />
+              </td>
+              <td>title</td>
+              <td>price</td>
+              <td>sales</td>
+              <td>
+                <img className="myGigs__delete" src="./img/delete.webp" alt="" />
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </section>
+  );
+};
 
-export default MyGigs
+export default MyGigs;
--- client/src/pages/Orders.tsx
+++ client/src/pages/Orders.tsx
@@ -1,8 +1,41 @@
-
 const Orders = () => {
   return (
-    <div>Orders</div>
-  )
-}
+    <div className="orders">
+      <div className="orders__container">
+        <div className="orders__title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Contact</th>
+          </tr>
+          {Array.from({ length: 5 }).map((_, i) => (
+            <tr key={i}>
+              <td>
+                <img
+                  className="orders__image"
+                  src={"/img/noavatar.webp"}
+                  alt=""
+                />
+              </td>
+              <td>title</td>
+              <td>price</td>
+              <td>
+                <img
+                  className="orders__message"
+                  src="./img/message.webp"
+                  alt=""
+                />
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default Orders
+export default Orders;
