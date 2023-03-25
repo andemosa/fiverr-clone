10798ee542c709109fbc82b5abbacb2710a07f10
andemosa <osayerieanderson@yahoo.com>
2023-03-25 13:43:57 +0100

    feat: Client - created hook for managing form state

---
+++ client/src/hooks/useForm.hook.ts
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function useForm() {
+  const [formState, setFormState] = useState({
+    submitting: false,
+    error: "",
+    successMsg: "",
+  });
+
+  const submittingForm = () => {
+    setFormState({
+      submitting: true,
+      error: "",
+      successMsg: "",
+    });
+  };
+
+  const formError = (error: string) => {
+    setFormState({
+      submitting: false,
+      error: error,
+      successMsg: "",
+    });
+  };
+
+  const formSuccess = (message: string) => {
+    setFormState({
+      submitting: false,
+      error: "",
+      successMsg: message,
+    });
+  };
+
+  return [formState, submittingForm, formError, formSuccess];
+}
