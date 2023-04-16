648ddcf244a4d9f63407461ccb2fa05e9296261a
andemosa <osayerieanderson@yahoo.com>
2023-04-16 18:16:37 +0100

    feat: Client - fetch messages from server in message page allow user to send message

--- client/src/pages/Message.tsx
+++ client/src/pages/Message.tsx
@@ -1,31 +1,109 @@
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom";
+import { FormEvent, ChangeEvent, useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const Message = () => {
+import DataFetcher from "@components/DataFetcher";
+import Button from "@components/Button";
+
+import { axiosInstance } from "@services/index";
+
+import { IMessageResponse } from "@customTypes/message";
+
+interface IPageProps extends IMessageResponse {
+  id: string;
+}
+
+interface IPostMessage {
+  conversation: string;
+  content: string;
+}
+
+const MessagePage = () => {
+  const { id } = useParams();
+
+  if (!id) return null;
+
+  return (
+    <DataFetcher<IMessageResponse>
+      url={`/messages/${id}`}
+      buildUI={(data) => <MessageDisplay {...data} id={id} />}
+    />
+  );
+};
+
+const MessageDisplay = ({ id, buyer, seller, messages }: IPageProps) => {
+  const [formData, setFormData] = useState({
+    content: "",
+    error: "",
+  });
+
+  const currentUser = JSON.parse(localStorage.getItem("currentUser")!)?.user;
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (message: IPostMessage) =>
+      axiosInstance.post("/messages", message),
+    onSuccess: () => {
+      queryClient.invalidateQueries([`/messages/${id}`]);
+      setFormData({
+        content: "",
+        error: "",
+      });
+    },
+    onError: (err: any) =>
+      setFormData((prev) => ({
+        ...prev,
+        error: err?.response?.data?.errorMessage,
+      })),
+  });
+
+  const { content, error } = formData;
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    mutation.mutate({ conversation: id, content });
+  };
+
   return (
     <section className="message">
       <div className="message__container">
         <span className="message__breadcrumbs">
-          <Link to="/messages">Messages</Link> &gt; John Doe &gt;
+          <Link to="/messages">Messages</Link> &gt;&nbsp;
+          {currentUser.isSeller ? buyer.username : seller.username} &gt;
         </span>
         <div className="message__messages">
-            {Array.from({ length: 5 }).map((_, i) => (
-              <div className={i % 2 ? "owner item" : "item"} key={i}>
-                <img
-                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam cupiditate, sunt voluptates tempora suscipit, neque molestiae excepturi sapiente fuga quidem modi quam accusamus consectetur quod facilis enim adipisci rerum nobis.</p>
-              </div>
-            ))}
-          </div>
+          {messages.map(({ _id, content, user }, i) => (
+            <div
+              className={user._id === currentUser._id ? "owner item" : "item"}
+              key={_id}
+            >
+              <img src={user.avatar} alt={user.username} />
+              <p>{content}</p>
+            </div>
+          ))}
+        </div>
         <hr />
-        <form>
-          <textarea placeholder="write a message" />
-          <button type="submit">Send</button>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            placeholder="write a message"
+            name="content"
+            value={content}
+            onChange={handleChange}
+          />
+          <Button isLoading={mutation.isLoading} disabled={mutation.isLoading}>
+            Send
+          </Button>
+          {mutation.isError && <pre>{error}</pre>}
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Message
+export default MessagePage;
