8c0f9d77654a78f750bc438a7e109a7bf1315d0b
andemosa <osayerieanderson@yahoo.com>
2023-05-02 21:40:25 +0100

    feat: Client - Pay page

--- client/src/pages/Pay.tsx
+++ client/src/pages/Pay.tsx
@@ -1,8 +1,51 @@
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import CheckoutForm from "@components/CheckoutForm";
+
+import { axiosInstance } from "@services/index";
+
+const STRIPE_PUBLIC_KEY = import.meta.env.VITE_STRIPE_PUBLIC_KEY;
+
+const stripePromise = loadStripe(STRIPE_PUBLIC_KEY);
 
 const Pay = () => {
-  return (
-    <div>Pay</div>
-  )
-}
+  const [clientSecret, setClientSecret] = useState("");
 
-export default Pay
+  const { id } = useParams();
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await axiosInstance.post(`/orders/${id}`);
+        setClientSecret(res.data.clientSecret);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    makeRequest();
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  } as const;
+
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className="pay">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+};
+
+export default Pay;
