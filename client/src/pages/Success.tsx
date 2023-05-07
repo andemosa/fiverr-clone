777e9280ed31dbee0f2f480dba9e805b38315246
andemosa <osayerieanderson@yahoo.com>
2023-05-07 13:31:06 +0100

    feat: Client - Success page

--- client/src/pages/Success.tsx
+++ client/src/pages/Success.tsx
@@ -1,8 +1,35 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { axiosInstance } from "@services/index";
 
 const Success = () => {
-  return (
-    <div>Success</div>
-  )
-}
+  const { search } = useLocation();
+  const navigate = useNavigate();
+  const params = new URLSearchParams(search);
+  const payment_intent = params.get("payment_intent");
 
-export default Success
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        await axiosInstance.patch("/orders", { payment_intent });
+        setTimeout(() => {
+          navigate("/orders");
+        }, 5000);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    makeRequest();
+  }, []);
+
+  return (
+    <div className="success">
+      Payment successful. You are being redirected to the orders page. Please do
+      not close the page
+    </div>
+  );
+};
+
+export default Success;
